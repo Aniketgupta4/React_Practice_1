e15027e0d29c4a10fe6b9ad4799577db0230f5dc
ANIKET GUPTA <136950558+Aniketgupta4@users.noreply.github.com>
2025-08-18 16:19:12 +0530

    Update script.js

--- chapter-4/script.js
+++ chapter-4/script.js
@@ -36,4 +36,67 @@ Reactroot.render(div1)
 const newelem = <h1>hello, baby</h1> // jsx format
 const div2 = React.createElement('div',{},newelem)
 const Reactroot2 = ReactDOM.createRoot(document.getElementById("root2"))
+
 Reactroot2.render(div2)
+
+// so ab apan jsx pe code likhayegai
+// multiple child aa shakte hai in <></> in single child 
+// for styling make style.css and link it in html file normal   
+
+// jsx expresions
+const name = "aniket"
+
+// object hao toh
+const obj = {
+    age:25,
+    salary:60000,
+}
+
+
+const elem = (
+    <>
+      <h1 id="1" className="first">Hello {name} , {obj.age} </h1>
+      <h1 id="2" money={23}>salary:{obj.salary}</h1>
+      {/* yaha pe apan money={23} as a number bhej ray hai and apan aaray obj kuch bhi bhej shakte hai -> and normal string mannta hai ye isko */}
+      <h1 id="3" className="second" style={{color:"white",backgroundColor:"blue"}}>Hello {name} , {obj.age} </h1>
+      {/* samjho isi liye style mai {{}} double aya hai ye object expect karta hai -> jsx mai javascript dalna hai so use 1 for {} this and 1 for object ke liye */}
+      {/* jsx ke andar javascript dalni hai so {} lagega and another for li mai object dalra hu -> and style ek object chahta hai so {} lagega and {} this for jsx to js*/}
+    </>
+)
+const React_root = ReactDOM.createRoot(document.getElementById('root2'))
+React_root.render(elem) 
+
+
+
+
+
+
+// react components
+// function based components -> funciton kaise likhte hai
+
+
+// function type1
+function greet(){
+    return <h1>hello , how are you</h1>
+}
+
+const funccom = greet()
+React_root.render(funccom)
+
+ 
+// function type2
+const meet = () =>{
+    return <h1>hello new one</h1>
+}
+
+const elem2 = meet()
+React_root.render(elem2)
+
+// or
+
+React_root.render(meet())
+
+
+// ek sath 2 function run in jsx -> {} lagana padega because jsx<></> ke ander javacript hai function call
+
+React_root.render(<>{meet()} {greet()}</>)
